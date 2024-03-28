e86acd85ce034019923cce9064eab5e8e66cfe65
Gai-Tosaka <tosakagai777@gmail.com>
2024-03-28 11:58:17 +0900

    idで検索する機能と例外処理を追加

--- src/main/java/com/example/nameservice/NameController.java
+++ src/main/java/com/example/nameservice/NameController.java
@@ -1,27 +1,34 @@
 package com.example.nameservice;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class NameController {
 
-    private NameMapper nameMapper;
+    private NameService nameService;
 
-    public NameController(NameMapper nameMapper) {
-        this.nameMapper = nameMapper;
+    public NameController(NameService nameService) {
+        this.nameService = nameService;
     }
 
     @GetMapping("/names")
-
     public List<Name> findNames(@RequestParam(required = false) String startsWith) {
         if (startsWith != null) {
-            return nameMapper.findByNameStartingWith(startsWith);
+            return nameService.findByNameStartingWith(startsWith);
         } else {
-            return nameMapper.findAll();
+            return nameService.findAll();
         }
+    }
+
+    @GetMapping("/names/{id}")
+    public Name findNameById(@PathVariable int id) {
+        return nameService.findById(id);
     }
 }
